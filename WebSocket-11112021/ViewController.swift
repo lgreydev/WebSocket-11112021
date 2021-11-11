e7747cddb2b1112ac17118e88a7c0fdddf1c86bb
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-12 00:10:26 +0200

    feat: add websocket

--- WebSocket-11112021/ViewController.swift
+++ WebSocket-11112021/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionWebSocketDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .cyan
+        
+        let session = URLSession(
+            configuration: .default,
+            delegate: self,
+            delegateQueue: OperationQueue()
+        )
+        guard let url = URL(string: "wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self") else { fatalError()}
+        let webSocket = session.webSocketTask(with: url)
+        webSocket.resume()
     }
 
-
+    func ping() {}
+    func close() {}
+    func send() {}
+    func receive() {}
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        <#code#>
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        <#code#>
+    }
 }
 
