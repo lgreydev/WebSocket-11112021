68087b9318b4a75e997e2220c016299bf8d5d33f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-12 00:26:27 +0200

    fix: update web socket

--- WebSocket-11112021/ViewController.swift
+++ WebSocket-11112021/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionWebSocketDelegate {
 
+    private var webSocket: URLSessionWebSocketTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .cyan
@@ -19,21 +21,32 @@ class ViewController: UIViewController, URLSessionWebSocketDelegate {
             delegateQueue: OperationQueue()
         )
         guard let url = URL(string: "wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self") else { fatalError()}
-        let webSocket = session.webSocketTask(with: url)
-        webSocket.resume()
+        webSocket = session.webSocketTask(with: url)
+        webSocket?.resume()
     }
 
-    func ping() {}
-    func close() {}
+    func ping() {
+        webSocket?.sendPing { error in
+            if let error = error {
+                print("Ping error: \(error)")
+            }
+        }
+    }
+                        
+                            
+    func close() {
+        webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
+    }
+    
     func send() {}
     func receive() {}
     
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
-        <#code#>
+        print("Did connect to socket")
     }
     
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-        <#code#>
+        print("Did close connection with reason")
     }
 }
 
