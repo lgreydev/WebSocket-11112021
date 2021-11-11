028bf8a3c46675c49c40090bb7ec0a134712034d
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-11 23:52:54 +0200

    Initial Commit

---
+++ WebSocket-11112021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebSocket-11112021
+//
+//  Created by Sergey Lukaschuk on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
